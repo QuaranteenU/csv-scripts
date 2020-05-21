9b67b8ac5fb09f4ec122b76c12e88bee540669fa
Rudhra Raveendran <rudytheninja@gmail.com>
2020-05-21 00:16:20 -0400

    add tts and playback

---
+++ generateTTS.py
@@ -0,0 +1,25 @@
+import csv
+import os
+from gtts import gTTS 
+from pydub import AudioSegment
+from io import BytesIO
+
+with open("data/final schedule with uuids.csv", encoding="utf-8") as scheduleFile:
+    graduates = [
+        {k: v for k, v in row.items()}
+        for row in csv.DictReader(scheduleFile, skipinitialspace=True)
+    ]
+
+for grad in graduates:
+    if grad['Senior quote?'] != '':
+        speech = gTTS(text=grad['Senior quote?'], lang='en')
+        speech.save("audio/{} tts.mp3".format(grad['Audio File Name']))
+
+        nameAudio = AudioSegment.from_mp3("audio/{}.mp3".format(grad['Audio File Name']))
+        ttsAudio = AudioSegment.from_mp3("audio/{} tts.mp3".format(grad['Audio File Name']))
+        combinedAudio = nameAudio + ttsAudio
+        finalAudio = combinedAudio[:min(30000, len(combinedAudio))]
+        finalAudio.export("audio/{}.mp3".format(grad['Audio File Name']), format="mp3")
+        
+        os.remove("audio/{} tts.mp3".format(grad['Audio File Name']))
+        print("Generated for {} ({})".format(grad['Your Full Name'], grad['Audio File Name']))
---
+++ playCeremonyAudio.py
@@ -0,0 +1,52 @@
+import csv
+import pymysql.cursors
+from datetime import datetime
+from apscheduler.schedulers.background import BackgroundScheduler
+from pydub import AudioSegment
+from pydub.playback import play
+
+sched = BackgroundScheduler()
+sched.start()
+
+def playAudio(filePath):
+    audio = AudioSegment.from_mp3(filePath)
+    play(audio)
+
+with open("data/final schedule with uuids.csv", encoding="utf-8") as scheduleFile:
+    graduates = [
+        {k: v for k, v in row.items()}
+        for row in csv.DictReader(scheduleFile, skipinitialspace=True)
+    ]
+
+emailToAudioFile = {}
+for grad in graduates:
+    emailToAudioFile[grad['Email Address']] = grad['Audio File Name']
+
+# Connect to the database
+connection = pymysql.connect(
+    host="localhost",
+    user="root",
+    password="asdfasdf",
+    db="graduation",
+    charset="utf8mb4",
+    autocommit=True,
+    cursorclass=pymysql.cursors.DictCursor,
+)
+
+try:
+    with connection.cursor() as cursor:
+        sql = "SELECT `*` FROM `graduates` WHERE NOT `graduated`"
+        cursor.execute(sql)
+        graduates = cursor.fetchall()
+        for grad in graduates:
+            num = emailToAudioFile[grad['email']]
+            sched.add_job(playAudio, 'date', run_date=grad['timeslot'], args=["audio/{}.mp3".format(num)])
+finally:
+    connection.close()
+
+sched.print_jobs()
+running = True
+while running:
+    inp = input('Send K for kill: ')
+    if inp == "K" or inp == "k":
+        running = False
